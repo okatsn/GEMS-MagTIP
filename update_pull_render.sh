04a4185d917beee1f2c5e9fd21b3a926618d1523
okatsn <okatsn@gmail.com>
2026-06-23 12:59:53 +0800

    Add a shortnote in shell script.

--- update_pull_render.sh
+++ update_pull_render.sh
@@ -1,5 +1,7 @@
 dvc pull
 git submodule update --init --remote
 cd GEMS-MagTIP-insider && dvc pull
+
+# For error "julia kernel not found", please update `index.qmd`.
 cd .. && quarto render --profile en && quarto render --profile zhtw
 
